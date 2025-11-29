9e483a1b18fc4c88178b03aa00bb7d904919998e
joskyitrax <thegeorgij@mail.ru>
2025-11-29 15:01:18 +0300

    Add files via upload

--- target/classes/db/data.sql
+++ target/classes/db/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (username, password, name) VALUES
-                                                 ('user1', '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Иван'),
-                                                 ('admin', '$2a$10$DOWB8eU56wv07QhGdG2VqO7Yq7yq7Yq7yq7yq7yq7yq7yq7yq7yq7', 'Админ');
+    ('user1', '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Иван')
+ON CONFLICT (username) DO NOTHING;
